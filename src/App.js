7d3e8cb40f07a220e6ae834da91996a5bf089df7
Tobifunmi <tobex13@gmail.com>
2022-05-16 21:27:34 +0100

    commit2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-// import Clients from './components/Clients/Clients';
 import Login from './components/Login/Login';
-// import SignUp from './components/Clients/SignUp';
+
 import Card from './components/Clients/Card';
 
 function App() {
@@ -9,8 +8,6 @@ function App() {
     <div className="App">
       {/* <h1>Finhalo</h1> */}
       <Login />
-      {/* <Clients/> */}
-      {/* <SignUp/> */}
       {/* <Card/> */}
     </div>
   );
