a963b810a7105779af8db724db17fbf851d2ccce
Tobifunmi <tobex13@gmail.com>
2022-05-19 21:56:17 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,26 @@
 //   Toolbar,
 //   Typography,
 // } from "@mui/material";
-// import { styled, useTheme } from "@mui/material/styles";
-// import MuiAppBar from "@mui/material/AppBar";
+// import React, {useState, useEffect} from "react";
+// import NotificationsNoneSharp from "@mui/icons-material/NotificationsNoneSharp";
 // import MenuIcon from "@mui/icons-material/Menu";
-// import AccountCircle from "@mui/icons-material/AccountCircle";
+// import { styled } from "@mui/material/styles";
+// import NotificationsNoneSharpIcon from "@mui/icons-material/NotificationsNoneSharp";
+// import Avatar from "@mui/material/Avatar";
+// import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+// import MuiAppBar from "@mui/material/AppBar";
 // import MailIcon from "@mui/icons-material/Mail";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
+// // import { useSelector } from "react-redux";
 // import MoreIcon from "@mui/icons-material/MoreVert";
-// import React from "react";
 
 // const drawerWidth = 240;
+
+// const StyledBadge = styled(Badge)(({ theme }) => ({
+//   "& .MuiBadge-badge": {
+//     right: 2,
+//     top: 4,
+//   },
+// }));
 
 // const AppBar = styled(MuiAppBar, {
 //   shouldForwardProp: (prop) => prop !== "open",
@@ -35,84 +45,11 @@
 //   }),
 // }));
 
-// export const Navbar = ({ title, open, setOpen }) => {
-//   const handleDrawerOpen = () => {
-//     setOpen(true);
-//   };
+// export const Navbar = ({ title }) => {
 
-//   return title !== "dashboard" ? (
-//     <Box sx={{ flexGrow: 1, zIndex: 10 }}>
-//       <AppBar position="static" color="inherit" open={open}>
-//         <Toolbar sx={{ height: 80 }}>
-//           <IconButton
-//             color="secondary"
-//             aria-label="open drawer"
-//             onClick={handleDrawerOpen}
-//             edge="start"
-//             sx={{
-//               marginRight: 5,
-//               ...(open && { display: "none" }),
-//             }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography
-//             variant="h4"
-//             color="secondary"
-//             component="div"
-//             sx={{ flexGrow: 1 }}
-//           >
-//             Finhalo
-//           </Typography>
 
-//           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show 4 new mails"
-//               color="secondary"
-//               href="/clients"
-//             >
-//               <Badge badgeContent={4} color="error">
-//                 <MailIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               aria-label="show 17 new notifications"
-//               color="secondary"
-//               href="/services"
-//             >
-//               <Badge badgeContent={17} color="error">
-//                 <NotificationsIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               edge="end"
-//               aria-label="account of current user"
-//               aria-haspopup="true"
-//               href="/login"
-//               color="secondary"
-//             >
-//               <AccountCircle />
-//             </IconButton>
-//           </Box>
-//           <Box sx={{ display: { xs: "flex", md: "none" } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show more"
-//               // aria-controls={mobileMenuId}
-//               aria-haspopup="true"
-//               // onClick={handleMobileMenuOpen}
-//               color="inherit"
-//             >
-//               <MoreIcon />
-//             </IconButton>
-//           </Box>
-//         </Toolbar>
-//       </AppBar>
-//     </Box>
-//   ) : (
+//   return title === "dashboard" ? (
+// : (
 //     <Box sx={{ flexGrow: 1 }}>
 //       <AppBar position="static" color="inherit">
 //         <Toolbar sx={{ height: 80 }}>
@@ -120,7 +57,7 @@
 //             variant="h4"
 //             color="secondary"
 //             component="div"
-//             sx={{ flexGrow: 1 }}
+//             sx={{ flexGrow: 1, fontWeight:800 }}
 //           >
 //             Finhalo
 //           </Typography>
@@ -149,56 +86,10 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-<<<<<<< HEAD
-
-import MailIcon from "@mui/icons-material/Mail";
 import React, {useState, useEffect} from "react";
-import NotificationsNoneSharp from "@mui/icons-material/NotificationsNoneSharp";
-
-export const Navbar = ({ title }) => {
-  const [name, setName] = useState("")
-  useEffect(() => {
-    let data = sessionStorage.getItem('data');
-    setName(data)  
-}, [])
-  return title === "dashboard" ? (
-< AppBar position="static" sx={{zIndex: 1}}>
-          <Toolbar
-            sx={{
-              backgroundColor:"white",
-              pr: '24px' // keep right padding when drawer closed
-            }}
-          >
-            
-            <Typography
-              component="h1"
-              variant="h6"
-              color="secondary.main"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              <h3>Welcome to Finhalo, {name}</h3>
-            </Typography>
-            <IconButton
-            size="large"
-            color="secondary"
-            >
-            <Badge badgeContent={4} color="warning">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-            <IconButton size="large" color="secondary">
-              <Badge badgeContent={4} color="warning">
-                <NotificationsNoneSharp />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-=======
-import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { styled } from "@mui/material/styles";
-import NotificationsNoneSharpIcon from "@mui/icons-material/NotificationsNoneSharp";
+import NotificationsNoneSharp from "@mui/icons-material/NotificationsNoneSharp";
 import Avatar from "@mui/material/Avatar";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 import MuiAppBar from "@mui/material/AppBar";
@@ -234,6 +125,11 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 export const Navbar = ({ setAuth, open, setOpen }) => {
+    const [name, setName] = useState("")
+  useEffect(() => {
+    let data = sessionStorage.getItem('data');
+    setName(data)  
+}, [])
   // const { user } = useSelector((state) => state.authReducer);
   const { user } = "useSelector((state) => state.authReducer)";
   const ch = window.location.pathname;
@@ -243,89 +139,40 @@ export const Navbar = ({ setAuth, open, setOpen }) => {
   };
   return ch !== "/" && ch !== "/login" ? (
     <div>
-      <AppBar position="fixed" color="inherit" open={open}>
-        <Toolbar sx={{ height: 80 }}>
-          <IconButton
-            color="secondary"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
+< AppBar position="static" sx={{zIndex: 1}}>
+          <Toolbar
             sx={{
-              marginRight: "36px",
-              ...(open && { display: "none" }),
+              backgroundColor:"white",
+              pr: '24px' // keep right padding when drawer closed
             }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            color="secondary"
-            sx={{
-              marginRight: "36px",
-              ...(open && { display: "none" }),
-            }}
-            variant="h4"
-          >
-            Finhalo
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-
-          <IconButton
-            size="large"
-            aria-label="show 4 new mails"
-            color="secondary"
-            href="/clients"
-          >
-            <StyledBadge badgeContent={4} color="warning">
-              <MailIcon />
-            </StyledBadge>
-          </IconButton>
-
-          <IconButton
-            sx={{ mr: 5 }}
-            color="secondary"
-            size="large"
-            href="/services"
-          >
-            <StyledBadge badgeContent={4} color="warning">
-              <NotificationsNoneSharpIcon />
-            </StyledBadge>
-          </IconButton>
-          <Avatar alt="" src="" sx={{ width: 50, height: 50, mr: 3 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Box sx={{ mr: 1 }}>
-              <Typography sx={{ color: "#000" }} variant="body2">
-                {user
-                  ? `
-              ${user.firstname} ${user.lastname}
-                `
-                  : "Login"}
-              </Typography>
-              <Typography variant="caption">
-                {user ? `${user.defaultRole}` : ""}
-              </Typography>
-            </Box>
-            <IconButton sx={{ mr: 3 }}>
-              <KeyboardArrowDownOutlinedIcon />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              // aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              // onClick={handleMobileMenuOpen}
-              color="inherit"
+            
+            <Typography
+              component="h1"
+              variant="h6"
+              color="secondary.main"
+              noWrap
+              sx={{ flexGrow: 1 }}
             >
-              <MoreIcon />
+              <h3>Welcome to Finhalo, {name}</h3>
+            </Typography>
+            <IconButton
+            size="large"
+            color="secondary"
+            >
+            <Badge badgeContent={4} color="warning">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+            <IconButton size="large" color="secondary">
+              <Badge badgeContent={4} color="warning">
+                <NotificationsNoneSharp />
+              </Badge>
             </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      ;
-    </div>
->>>>>>> fd817b9a905b8aa83fb0b96c08c9e0f39c6c519c
-  ) : (
+          </Toolbar>
+        </AppBar>
+        </div>
+  )  : (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="inherit">
         <Toolbar sx={{ height: 80 }}>
@@ -333,7 +180,7 @@ export const Navbar = ({ setAuth, open, setOpen }) => {
             variant="h4"
             color="secondary"
             component="div"
-            sx={{ flexGrow: 1, fontWeight:800 }}
+            sx={{ flexGrow: 1 }}
           >
             Finhalo
           </Typography>
